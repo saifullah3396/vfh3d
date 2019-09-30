53c3ce3c9d5f47ee00c563e002725dfc50b09c70
saifullah3396 <Saifullah3396@gmail.com>
2019-09-30 10:33:01 +0500

    Added a bounding box node iterator but first need to visualize and see if this is what we want.

--- include/vfh3d/polar_histogram.h
+++ include/vfh3d/polar_histogram.h
@@ -30,6 +30,13 @@ class PolarHistogram {
     // in paper, it is robot_radius + safety_radius + voxel_size. voxel_size should be voxel_radius?
     auto voxel_enlargement_radius = 
       v->getRadius() + v->getSafetyRadius() + oc_tree_->getResolution() * 0.5;
+
+    // iterate through the nodes in the box
+    auto begin = oc_tree_->begin_leafs_bbx(min_hist_range, max_hist_range);
+    for(auto iter = begin; iter!= oc_tree_->end_leafs_bbx(); ++iter)
+    {
+      // first test if this is correct
+    }
   }
 
 private:
