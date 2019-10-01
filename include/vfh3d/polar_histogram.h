1b1d93a9a11a9a79ac25cdba1597891ce494e465
saifullah3396 <Saifullah3396@gmail.com>
2019-10-01 14:00:18 +0500

    Added HistVoxel class that updates histogram bins from octomap voxel occupancy. The HistVoxel deals currently implements stage 1-2 of vfh3d algorithm. Histogram binarization was also done with t_high and t_low arbitrarilty set for now.

--- include/vfh3d/polar_histogram.h
+++ include/vfh3d/polar_histogram.h
@@ -6,6 +6,7 @@
 #include <vfh3d/utils.h>
 #include <geometry_msgs/Pose.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <eigen3/Eigen/Dense>
 
 using namespace octomap;
 
@@ -15,22 +16,130 @@ class PolarHistogram {
  public:
   PolarHistogram(
     const std::shared_ptr<OcTree>& oc_tree,
-    const std::weak_ptr<VehicleState>& vehicle_state);
+    const std::weak_ptr<VehicleState>& vehicle_state,
+    const double& resolution);
   ~PolarHistogram() {}
 
-  void update(const double& max_plan_range, const double& hist_resolution);
+  void update(const double& max_plan_range);
+  bool inSphere(const octomap::point3d& p, const double& radius);
 
   visualization_msgs::MarkerArray getBbxMarkers() {
     return bbx_markers_;
   }
 
+  visualization_msgs::MarkerArray getDataMarkers() {
+    return data_markers_;
+  }
+
 private:
+
+  class HistVoxel {
+  public:
+    HistVoxel(
+      const octomap::point3d& position,
+      const float& occ_value,
+      PolarHistogram* histogram) :
+      position_(position),
+      occ_value_(occ_value),
+      histogram_(histogram)
+    {
+      vehicle_position_ = histogram_->vehicle_state_.lock()->center();
+      dist_from_vehicle_ = (position - vehicle_position_).norm();
+    }
+
+    double dist() const { return dist_from_vehicle_; }
+    void updateHist() {
+      // compute weight
+      computeWeight();
+
+      auto diff = position_ - vehicle_position_;
+      // azimuth angle histogram x - coordinate
+      int bz = floor((int)(atan2(diff.y(), diff.x()) / histogram_->resolution_)) + histogram_->data_.cols() / 2.0;
+
+      // elevation angle histogram y - coordinate
+      int be = 
+        floor((int)(
+          atan2(diff.z(), sqrt(diff.x()*diff.x() + diff.y()*diff.y())) /
+          histogram_->resolution_)) + 
+          histogram_->data_.rows() / 2.0;
+      
+      be = std::min(std::max(be, 0), (int)histogram_->data_.rows());
+      bz = std::min(std::max(bz, 0), (int)histogram_->data_.cols());
+      // each voxel affects multiple histogram bins within its size range
+      auto oneOverRes = 1.0 / histogram_->resolution_;
+      min_max_angle_ = floor(oneOverRes * asin(std::min(histogram_->enlargement_radius_ / dist_from_vehicle_, 1.0)));
+      auto r_min = std::max((int)(be - min_max_angle_), 0);
+      auto r_max = std::min((int)(be + min_max_angle_), (int)histogram_->data_.rows());
+      auto c_min = std::max((int)(bz - min_max_angle_), 0);
+      auto c_max = std::min((int)(bz + min_max_angle_), (int)histogram_->data_.cols());
+      for (int r = r_min; r < r_max; ++r) {
+        for (int c = c_min; c < c_max; ++c) {
+          if (histogram_->data_(r, c) == histogram_->data_(r, c))
+            histogram_->data_(r, c) += weight_;
+          else 
+            histogram_->data_(r, c) = weight_;
+        }
+      }
+    }
+
+  private:
+    void computeWeight() {
+      auto min_dist_from_vehicle = dist_from_vehicle_ - histogram_->enlargement_radius_;
+      weight_ = pow(occ_value_, 2) * (histogram_->const_a_ - histogram_->const_b_ * min_dist_from_vehicle);
+    }
+
+    octomap::point3d position_, vehicle_position_;
+    float occ_value_;
+    double dist_from_vehicle_;
+    double min_max_angle_;
+    double weight_;
+    PolarHistogram* histogram_;
+  };
+
+  double histSum() {
+    double sum = 0.0;
+    for (int i = 0; i < data_.rows(); ++i) {
+      for (int j = 0; j < data_.cols(); ++j) {
+        const auto& d = data_(i, j);
+        if (d != d)
+          continue;
+        sum += d;
+      }
+    }
+    return sum;
+  }
+
+  double histMean() {
+    return histSum() / data_.size();
+  }
+
+  double histStd(const double& mean) {
+    double sum = 0.0;
+    for (int i = 0; i < data_.rows(); ++i) {
+      for (int j = 0; j < data_.cols(); ++j) {
+        const auto& d = data_(i, j);
+        if (d != d)
+          continue;
+        sum += pow(d - mean, 2);
+      }
+    }
+    return sqrt(sum / data_.size() - 1);
+  }
+
+  double resolution_;
+  size_t height_, width_;
+  double enlargement_radius_;
+  double voxel_radius_;
+  double const_a_, const_b_;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> data_;
 
   std::shared_ptr<OcTree> oc_tree_;
   std::weak_ptr<VehicleState> vehicle_state_;
   int marker_id_;
   visualization_msgs::Marker cell_marker_;
+  visualization_msgs::Marker point_marker_;
   visualization_msgs::MarkerArray bbx_markers_;
+  visualization_msgs::MarkerArray data_markers_;
 };
 
 }
--- src/polar_histogram.cpp
+++ src/polar_histogram.cpp
@@ -4,32 +4,50 @@ namespace vfh3d {
 
 PolarHistogram::PolarHistogram(
   const std::shared_ptr<OcTree>& oc_tree,
-  const std::weak_ptr<VehicleState>& vehicle_state) :
+  const std::weak_ptr<VehicleState>& vehicle_state,
+  const double& resolution) :
   oc_tree_(oc_tree),
-  vehicle_state_(vehicle_state)
+  vehicle_state_(vehicle_state),
+  resolution_(resolution)
 {
   cell_marker_.header.frame_id = "map";
   cell_marker_.ns = "fbe3d";
   cell_marker_.type = visualization_msgs::Marker::CUBE;
   cell_marker_.action = visualization_msgs::Marker::MODIFY;
   cell_marker_.color.a = 0.5;
-  cell_marker_.lifetime = ros::Duration(0.1);
+  cell_marker_.lifetime = ros::Duration(10);
   auto res = oc_tree->getResolution();
   cell_marker_.scale.x = res;
   cell_marker_.scale.y = res;
   cell_marker_.scale.z = res;
+  point_marker_ = cell_marker_;
+  point_marker_.type = visualization_msgs::Marker::CUBE;
+  point_marker_.scale.x = res / 3.0;
+  point_marker_.scale.y = res / 3.0;
+  point_marker_.scale.z = res / 3.0;
+  point_marker_.color.r = 1.0;
   marker_id_ = 0;
+
+  auto v = vehicle_state_.lock();
+  // in paper, it is robot_radius + safety_radius + voxel_size. voxel_size should be voxel_radius?
+  voxel_radius_ = oc_tree_->getResolution() * 0.5;
+  enlargement_radius_ = 
+    v->getRadius() + v->getSafetyRadius() + voxel_radius_;
+  height_ = size_t(ceil(M_PI / resolution_)); // steps in radians
+  width_ = size_t(ceil(2 * M_PI / resolution_)); // steps in radians
+  data_.resize(height_, width_);
+  data_.setConstant(NAN);
+
+  const_a_ = 0.5;
+  const_b_ = (const_a_ - 1) / pow((v->getRadius() - 1.0) / 2.0, 2);
 }
 
-void PolarHistogram::update(const double& max_plan_range, const double& hist_resolution) {
+void PolarHistogram::update(const double& max_plan_range) {
   bbx_markers_.markers.clear();
   auto v = vehicle_state_.lock();
   auto range = octomath::Vector3(max_plan_range, max_plan_range, max_plan_range);
   auto min_hist_range = v->min() - range;
   auto max_hist_range = v->max() + range;
-  // in paper, it is robot_radius + safety_radius + voxel_size. voxel_size should be voxel_radius?
-  auto voxel_enlargement_radius = 
-    v->getRadius() + v->getSafetyRadius() + oc_tree_->getResolution() * 0.5;
 
   std_msgs::ColorRGBA color;
   color.a = 1;
@@ -38,15 +56,90 @@ void PolarHistogram::update(const double& max_plan_range, const double& hist_res
   // iterate through the nodes in the box
   auto begin = oc_tree_->begin_leafs_bbx(min_hist_range, max_hist_range);
   auto end = oc_tree_->end_leafs_bbx();
-  marker_id_ = 0;        
+  marker_id_ = 0;
+
+  // reset histogram
+  data_.setConstant(NAN);
   for(auto iter = begin; iter!= end; ++iter)
   {
+    auto voxel = HistVoxel(iter.getCoordinate(), iter->getOccupancy(), this);
+    if (voxel.dist() > max_plan_range) // not in sphere
+      continue;
+    // add weight of each voxel in histogram bins affected by it
+    voxel.updateHist();
+
+    if (iter->getValue() > 0) { // high probablity of occupied
+      color.r = 0;
+      color.b = 1;
+      cell_marker_.color = color;
+    } else { // else
+      color.r = 1;
+      color.b = 0;
+      cell_marker_.color = color;
+    }
+
     auto coord = iter.getCoordinate();
     cell_marker_.pose.position.x = coord.x();
     cell_marker_.pose.position.y = coord.y();
     cell_marker_.pose.position.z = coord.z();
     cell_marker_.id = marker_id_++;
     bbx_markers_.markers.push_back(cell_marker_);
+  }
+
+  data_markers_.markers.clear();
+  auto t_high = 0.6;
+  auto t_low = 0.4;
+  for (int i = 0; i < data_.rows(); ++i) {
+    for (int j = 0; j < data_.cols(); ++j) {
+      const auto& d = data_(i, j);
+      if (d != d) {
+        point_marker_.color.a = 1.0;
+        point_marker_.color.b = 0.0;
+        point_marker_.color.r = 1.0;
+        point_marker_.color.g = 0.0;
+        auto x = (i - data_.rows() / 2) * max_plan_range * resolution_;
+        auto y = (j - data_.cols() / 2) * max_plan_range * resolution_;
+        point_marker_.pose.position.x = v->center().x() + x * cos(v->getYaw()) - y * sin(v->getYaw());
+        point_marker_.pose.position.y = v->center().y() + x * sin(v->getYaw()) + y * cos(v->getYaw());
+        point_marker_.pose.position.z = v->center().z();
+        point_marker_.pose.orientation.x = v->getPose().getRotation().x();
+        point_marker_.pose.orientation.y = v->getPose().getRotation().y();
+        point_marker_.pose.orientation.z = v->getPose().getRotation().z();
+        point_marker_.pose.orientation.w = v->getPose().getRotation().w();
+        point_marker_.id = marker_id_++;
+        data_markers_.markers.push_back(point_marker_);
+        continue;
+      }
+      if (d > t_high) {
+        data_(i, j) = 1.0;
+      } else if (d < t_low) {
+        data_(i, j) = 0.0;
+      } else {
+        data_(i, j) = fabs(d - t_low) < fabs(d - t_high) ? 0.0 : 1.0;
+      }
+      if (data_(i, j) > 0.5) {
+        point_marker_.color.a = 1.0;
+        point_marker_.color.b = 1.0;
+        point_marker_.color.r = 1.0;
+        point_marker_.color.g = 1.0;
+      } else {
+        point_marker_.color.a = 1.0;
+        point_marker_.color.b = 0.0;
+        point_marker_.color.r = 0.0;
+        point_marker_.color.g = 0.0;
+      }
+      auto x = (i - data_.rows() / 2) * max_plan_range * resolution_;
+      auto y = (j - data_.cols() / 2) * max_plan_range * resolution_;
+      point_marker_.pose.position.x = v->center().x() + x * cos(v->getYaw()) - y * sin(v->getYaw());
+      point_marker_.pose.position.y = v->center().y() + x * sin(v->getYaw()) + y * cos(v->getYaw());
+      point_marker_.pose.position.z = v->center().z();
+      point_marker_.pose.orientation.x = v->getPose().getRotation().x();
+      point_marker_.pose.orientation.y = v->getPose().getRotation().y();
+      point_marker_.pose.orientation.z = v->getPose().getRotation().z();
+      point_marker_.pose.orientation.w = v->getPose().getRotation().w();
+      point_marker_.id = marker_id_++;
+      data_markers_.markers.push_back(point_marker_);
+    }
   }
 }
 
